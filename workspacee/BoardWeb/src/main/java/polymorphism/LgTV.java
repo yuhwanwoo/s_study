fab1bd6416321449abb3aed47bc5339229a0cb26
yuhwanwoo <6514687@naver.com>
2020-03-02 16:26:51 +0900

    Add files via upload

---
+++ workspacee/BoardWeb/src/main/java/polymorphism/LgTV.java
@@ -0,0 +1,29 @@
+package polymorphism;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("tv")  //component선언하면 기본생성자는 호출하는듯
+public class LgTV implements TV{
+	@Autowired
+	private Speaker speaker;
+	
+	public LgTV() {
+		System.out.println("===> LgTV 객체 생성");
+	}
+	
+	public void powerOn() {
+		System.out.println("LgTV---전원 켠다.");
+	}
+	
+	public void powerOff() {
+		System.out.println("LgTV---전원 끈다.");
+	}
+	public void volumeUp() {
+		speaker.volumeUp();
+	}
+	public void volumeDown() {
+		speaker.volumeDown();
+	}
+}
